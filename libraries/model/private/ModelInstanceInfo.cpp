9c3ded1c3c58644cfcdc1a11509e7c64de1db876
cledant <cledant@student.42.fr>
2021-10-10 21:37:01 +0200

    Fix Instance matrix computation

--- libraries/model/private/ModelInstanceInfo.cpp
+++ libraries/model/private/ModelInstanceInfo.cpp
@@ -9,16 +9,16 @@ computeInstanceMatrix(glm::vec3 const &modelCenter,
                       ModelInstanceInfo const &info)
 {
     auto instance_matrix = glm::mat4(1.0f);
-    instance_matrix = glm::scale(instance_matrix, info.scale);
-    instance_matrix = glm::translate(instance_matrix, -modelCenter);
 
-    instance_matrix =
-      glm::rotate(instance_matrix, info.pitch, glm::vec3(1.0f, 0.0f, 0.0f));
+    instance_matrix = glm::translate(instance_matrix, info.position);
     instance_matrix =
       glm::rotate(instance_matrix, info.yaw, glm::vec3(0.0f, 1.0f, 0.0f));
     instance_matrix =
+      glm::rotate(instance_matrix, info.pitch, glm::vec3(1.0f, 0.0f, 0.0f));
+    instance_matrix =
       glm::rotate(instance_matrix, info.roll, glm::vec3(0.0f, 0.0f, 1.0f));
-    instance_matrix = glm::translate(instance_matrix, info.position);
+    instance_matrix = glm::scale(instance_matrix, info.scale);
+    instance_matrix = glm::translate(instance_matrix, -modelCenter);
 
     return (instance_matrix);
 }
